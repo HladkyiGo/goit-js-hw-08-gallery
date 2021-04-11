a5090fe99b8fb53829a5bf2ba1ef2ae31fdbefc8
HladkyiGo <74014701+HladkyiGo@users.noreply.github.com>
2021-04-11 11:02:50 +0200

    7dz

--- src/gallery-items.js
+++ src/gallery-items.js
@@ -1,4 +1,5 @@
-export default [
+
+  const images=[
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
@@ -63,3 +64,71 @@ export default [
     description: 'Lighthouse Coast Sea',
   },
 ];
+const refs = {
+  gallery: document.querySelector(".js-gallery"),
+  image: document.createElement("img"),
+  lightbox: document.querySelector(".lightbox"),
+  btn: document.querySelector('[data-action="close-lightbox"]'),
+  modal: document.querySelector(".lightbox__content"),
+  lightbox__image: document.querySelector(".lightbox__image"),
+};
+
+const createGalleryItem = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+<a
+  class="gallery__link"
+  href=${original}
+>
+  <img
+    class="gallery__image"
+    src=${preview}
+    data-source=${original}
+    alt=${description}
+  />
+</a>
+</li>`;
+const galleryMarkup = images.reduce(
+  (acc, item) => acc + createGalleryItem(item),
+  ""
+  
+);
+console.log(galleryMarkup);
+refs.gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+refs.image.classList.add("gallery__image");
+
+refs.gallery.addEventListener("click", onGalleryClick);
+refs.btn.addEventListener("click", onClickHandlerClose);
+refs.modal.addEventListener("click", closeLightbox);
+
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  if (e.target.nodeName === "IMG") {
+    refs.lightbox.classList.add("is-open");
+    refs.lightbox__image.src = e.target.getAttribute("data-source");
+    refs.lightbox__image.alt = e.target.alt;
+  }
+  window.addEventListener("keyup", clickKey);
+}
+
+function onClickHandlerClose(e) {
+  e.preventDefault(); 
+  refs.lightbox.classList.remove("is-open");
+  refs.lightbox__image.src = '';
+  refs.lightbox__image.alt = '';
+  window.removeEventListener("keyup", clickKey);
+}
+
+function closeLightbox(event) {
+  if (event.target === event.currentTarget) {
+    onClickHandlerClose();
+  }
+}
+
+function clickKey(event) {
+  if (event.code === "Escape") {
+    onClickHandlerClose();
+  }
+}
